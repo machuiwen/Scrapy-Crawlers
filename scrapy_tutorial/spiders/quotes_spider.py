bde676a886f006daf0c8bba8c7f7855305cb32c9
Chuiwen Ma <machuiwen@gmail.com>
2017-01-31 22:37:26 -0800

    Follow links recursively

--- scrapy_tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/spiders/quotes_spider.py
@@ -6,8 +6,7 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://quotes.toscrape.com/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -19,3 +18,8 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('span small::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page, callback=self.parse)
